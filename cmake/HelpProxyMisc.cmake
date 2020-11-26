40d71d1626f0a3795ec0ad6256c830a7b24cd322
Rogonov Stepan <sr@uveon.ru>
2020-11-26 22:18:26 +0300

    cmake port

---
+++ cmake/HelpProxyMisc.cmake
@@ -0,0 +1,56 @@
+include(CheckFunctionExists)
+include(CheckLibraryExists)
+include(CheckIncludeFile)
+
+function(global_proxy_list LISTNAME)
+    set(${LISTNAME} "" CACHE INTERNAL "${LISTNAME}")
+endfunction(global_proxy_list)
+
+function(global_proxy_list_append LISTNAME NEWELEMENT)
+    set(_NEW_LIST)
+
+    # double referencing
+    # LISTNAME contains `name` of global list
+    list(APPEND _NEW_LIST "${${LISTNAME}}")
+    list(APPEND _NEW_LIST "${NEWELEMENT}")
+
+    set(${LISTNAME} "${_NEW_LIST}" CACHE INTERNAL "${LISTNAME}")
+endfunction(global_proxy_list_append)
+
+function(proxy_require_lib_with_func FUNC_NAME LIB_NAME VAR_NAME LIBS_LISTNAME DEFS_LISTNAME)
+    set(CMAKE_REQUIRED_LIBRARIES "${LIB_NAME}")
+    check_function_exists("${FUNC_NAME}" "_FUNC_IN_LIB_GLOBAL_${FUNC_NAME}")
+    if ("${_FUNC_IN_LIB_GLOBAL_${FUNC_NAME}}")
+        set(IS_EXISTS TRUE)
+    else ()
+        check_library_exists("${LIB_NAME}" "${FUNC_NAME}" "" IS_EXISTS)
+    endif()
+    set(CMAKE_REQUIRED_LIBRARIES)
+
+    set(${VAR_NAME} ${IS_EXISTS} CACHE INTERNAL "${VAR_NAME}")
+    if (${IS_EXISTS})
+        global_proxy_list_append(${LIBS_LISTNAME} ${LIB_NAME})
+        global_proxy_list_append(${DEFS_LISTNAME} ${VAR_NAME})
+    else ()
+        message(SEND_ERROR "Cannot find library \"${LIB_NAME}\" with function \"${FUNC_NAME}\"")
+    endif ()
+endfunction(proxy_require_lib_with_func)
+
+function(proxy_option_to_definition _OPTION _DEFINITION DEFS_LISTNAME)
+    if (${${_OPTION}})
+        global_proxy_list_append(${DEFS_LISTNAME} ${_DEFINITION})
+    endif()
+endfunction(proxy_option_to_definition)
+
+function(proxy_require_header HFILE VAR_NAME DEFS_LISTNAME)
+    check_include_file(${HFILE} IS_EXISTS)
+
+    set(${VAR_NAME} ${IS_EXISTS} CACHE INTERNAL "${VAR_NAME}")
+    if (${IS_EXISTS})
+        global_proxy_list_append(${DEFS_LISTNAME} ${VAR_NAME})
+    else ()
+        message(SEND_ERROR "Cannot find header \"${HFILE}\"")
+    endif ()
+endfunction(proxy_require_header)
+
+
